085cc9269798db63c81d8ed4b21006bf30ea87f6
Barry Wallis <barry.wallis@outlook.com>
2022-12-03 22:36:36 -0800

    Completed Day3B.

--- Day3B/Program.cs
+++ Day3B/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 /*
 --- Day 3: Rucksack Reorganization ---
 One Elf has the important job of loading all of the rucksacks with supplies for the jungle journey.
@@ -46,38 +48,90 @@ In the above example, the priority of the item type that appears in both compart
 
 Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those 
 item types?
+
+--- Part Two ---
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. 
+For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That
+is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, 
+and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the 
+badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which 
+item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge 
+item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their 
+badges. In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) 
+for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of 
+those item types?
 */
 
 using CommunityToolkit.Diagnostics;
 
 int result = 0;
-string? line;
-while ((line = Console.ReadLine()) != null)
+List<HashSet<char>> lines;
+while ((lines = GetRuscksackContents()).Count != 0)
 {
-    List<HashSet<char>> rucksacks = GetRuscksackContents(line);
-    IEnumerable<char> intersection = rucksacks[0].Intersect(rucksacks[1]);
+    IEnumerable<char> intersection = RucksacksIntersection(lines);
     result += GetItemValue(intersection);
 }
-
 Console.WriteLine(result);
+
+IEnumerable<char> RucksacksIntersection(List<HashSet<char>> lines)
+{
+    Guard.IsNotEmpty(lines);
+
+    IEnumerable<char> result = lines[0];
+    foreach (HashSet<char> line in lines.Skip(1))
+    {
+        result = result.Intersect(line);
+    }
+
+    Guard.IsEqualTo(result.Count(), 1);
+    return result;
+}
+
 
 int GetItemValue(IEnumerable<char> intersection)
 {
     Guard.IsEqualTo(intersection.Count(), 1);
+    Guard.IsTrue(char.IsLetter(intersection.First()));
+
     char item = intersection.First();
     return char.IsLower(item) ? item - 'a' + 1 : item - 'A' + 27;
 }
 
-List<HashSet<char>> GetRuscksackContents(string line)
+List<HashSet<char>> GetRuscksackContents()
 {
-    Guard.IsNotNullOrWhiteSpace(line);
-    Guard.IsEqualTo(line.Length % 2, 0);
-    Guard.IsTrue(line.All(char.IsLetter));
-
-    List<HashSet<char>> rucksacks = new()
+    List<HashSet<char>> rucksacks = new();
+    if (Console.In.Peek() > -1)
     {
-        new HashSet<char>(line[..(line.Length / 2)]),
-        new HashSet<char>(line[(line.Length / 2)..])
-    };
+        rucksacks = new()
+        {
+            new HashSet<char>(Console.ReadLine() ?? throw new EndOfStreamException("Unexpected end of file")),
+            new HashSet<char>(Console.ReadLine() ?? throw new EndOfStreamException("Unexpected end of file")),
+            new HashSet<char>(Console.ReadLine() ?? throw new EndOfStreamException("Unexpected end of file")),
+        };
+    }
     return rucksacks;
 }
